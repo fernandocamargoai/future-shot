dc984fd918ffd35cd508c82eed9d6ae49c8f0133
Fernando Camargo <fernando.camargo.ai@gmail.com>
2023-04-01 13:29:04 -0700

    feat: adds a wandb logger that deletes the local files in the end

---
+++ src/future_shot/extra/wandb.py
@@ -0,0 +1,12 @@
+import shutil
+
+from pytorch_lightning.loggers import WandbLogger
+
+
+class WandbWithLocalFilesDeletionLogger(WandbLogger):
+    """WandbLogger with local files deletion."""
+
+    def finalize(self, status: str) -> None:
+        super().finalize(status)
+
+        shutil.rmtree(self.save_dir)
