f1bfbf9adff934d63f2bc4cba2da0802d3ba9e17
Fernando Camargo <fernando.camargo.ai@gmail.com>
2023-06-17 18:05:58 -0700

    feat: adds drop_rate to timm

--- src/future_shot/extra/timm/model.py
+++ src/future_shot/extra/timm/model.py
@@ -12,12 +12,16 @@ class TimmModel(nn.Module):
         pretrained: bool = True,
         dense_layer_dim: Optional[int] = None,
         image_field: str = "img",
+        drop_rate: float = 0.0
     ):
         super().__init__()
         self._model = timm.create_model(
             model_name, pretrained=pretrained, num_classes=0 if dense_layer_dim is None else dense_layer_dim
         )
+        self._dropout = nn.Dropout(drop_rate)
         self._image_field = image_field
 
     def forward(self, batch: Dict[str, Any]) -> torch.Tensor:
-        return self._model(batch[self._image_field])
+        features = self._model(batch[self._image_field])
+        features = self.drop(features)
+        return features
